7ace115a359a003e4a16b9f9f2cb11243f13a5f2
Lucas Oliveira <lucas.l28@aluno.ifsc.edu.br>
2024-09-30 08:54:37 -0300

    IMC COmpleto
    to doidoooo

--- IMC/app/src/main/java/com/example/contador/MainActivity2.java
+++ IMC/app/src/main/java/com/example/contador/MainActivity2.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -15,22 +16,24 @@ public class MainActivity2 extends AppCompatActivity {
     TextView IMC;
     TextView sla;
 
+    ImageView image;
 
-    @SuppressLint("MissingInflatedId")
+
+    @SuppressLint({"MissingInflatedId", "ResourceType", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         IMC = findViewById(R.id.IMC2);
         sla = findViewById(R.id.ResultadoF);
+        image = findViewById(R.id.imageView);
 
 
                 Bundle bundle = getIntent().getExtras();
                 String x = bundle.getString("x");
-                TextView textview = findViewById(R.id.Peso);
 
-                 String y = bundle.getString("y");
-                TextView textview2 = findViewById(R.id.Altura);
+                String y = bundle.getString("y");
+
 
                 float p;
                 p = Float.parseFloat(x);
@@ -42,23 +45,31 @@ public class MainActivity2 extends AppCompatActivity {
                 resul = p/(A*A);
 
                 IMC.setText("Calculo: "+resul);
+
                 if (resul <= 18.5){
                     sla.setText("Abaixo do peso");
+                    image.setImageResource(R.drawable.abaixopeso);
+
                 }
                 else if(resul>18.5 && resul<24.9){
                     sla.setText("Peso normal");
+                    image.setImageResource(R.drawable.normal);
                 }
                 else if(resul>25 && resul<29.9){
                     sla.setText("Sobrepeso");
+                    image.setImageResource(R.drawable.sobrepeso);
                 }
                 else if(resul>30 && resul<34.9){
                     sla.setText("Obesidade Grau I");
+                    image.setImageResource(R.drawable.obesidade1);
                 }
                 else if(resul>35 && resul<39.9){
                     sla.setText("Obesidade Grau II");
+                    image.setImageResource(R.drawable.obesidade2);
                 }
                 else {
                     sla.setText("Obesidade Grau III");
+                    image.setImageResource(R.drawable.obesidade3);
                 }
 
             }
